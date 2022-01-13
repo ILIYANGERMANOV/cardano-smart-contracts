e8e38d02d99cf76cd74d701e6be762e8515e8a6e
Iliyan Germanov <iliyan@qredo.com>
2022-01-13 19:04:45 +0200

    Add imports

--- nft-contest/src/NFTContest.hs
+++ nft-contest/src/NFTContest.hs
@@ -33,12 +33,26 @@ import           Ledger.Ada           as Ada
 import           Playground.Contract  (IO, ensureKnownCurrencies, printSchemas, stage, printJson)
 import           Playground.TH        (mkKnownCurrencies, mkSchemaDefinitions)
 import           Playground.Types     (KnownCurrency (..))
-import           Plutus.Contract
 import qualified PlutusTx
 import           PlutusTx.Prelude     hiding (unless)
 import qualified Prelude              as P
 import           Schema               (ToSchema)
 import           Text.Printf          (printf)
+import           Control.Monad                hiding (fmap)
+import           Data.Monoid                  (Last (..))
+import           Plutus.Contract              as Contract
+import           Plutus.Contract.StateMachine
+import           PlutusTx.Prelude             hiding (Semigroup(..), check, unless)
+import           Ledger                       hiding (singleton)
+import           Ledger.Ada                   as Ada
+import           Ledger.Constraints           as Constraints
+import qualified Ledger.Typed.Scripts         as Scripts
+import           Ledger.Value
+import           Prelude                      (Semigroup (..), Show (..), uncurry)
 
-
+hello:: P.String
 hello = "Hello from NFTContest.hs"
+
+-- TODO Define validator
+
+-- Define Contracts
