31221131275284a5beec934dec7cb7c01664a13d
Iliyan Germanov <iliyan@qredo.com>
2022-01-18 18:22:21 +0200

    WIP: Compile to Plutus

--- ivy-token/src/IvyToken.hs
+++ ivy-token/src/IvyToken.hs
@@ -37,9 +37,29 @@
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+
+
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
 
 module IvyToken(
-    defGovernance
+    defGovernance,
+    Proposal (..),
+    Governance (..),
+    VoteType (..)
 ) where
 
 import           Control.Monad        hiding (fmap)
@@ -62,7 +82,6 @@ import           Text.Printf          (printf)
 import           Control.Monad                hiding (fmap)
 import           Data.Monoid                  (Last (..))
 import           Plutus.Contract              as Contract
-import           Plutus.Contract.StateMachine
 import           PlutusTx.Prelude             hiding (Semigroup(..), check, unless)
 import           Ledger                       hiding (singleton)
 import           Ledger.Ada                   as Ada
@@ -76,9 +95,20 @@ import           Plutus.Contract.Request
 import           Wallet.Emulator.Types
 import           PlutusTx.Enum
 import           Data.Map             as Map
+import           Ledger.Constraints
 
 
 -- Data
+data Governance = Governance {
+    pVotesThreshold     :: Integer,
+    gVotesThreshold     :: Integer,
+    maxProposalPrice    :: Integer,
+    votersRewardPercent :: Integer
+} deriving (Show, Generic, FromJSON, ToJSON, ToSchema)
+
+PlutusTx.unstableMakeIsData ''Governance
+PlutusTx.makeLift ''Governance
+
 data Proposal = 
     Proposal {
         id        :: Integer,
@@ -86,37 +116,48 @@ data Proposal =
         tag       :: String,
         url       :: String,
         price     :: Integer
-    } | 
+    } 
+    | 
     GovernanceUpdate {
         id              :: Integer,
         newGovernance   :: Governance
     } 
-    deriving (Show)
+    deriving (Show, Generic, FromJSON, ToJSON)
+
+PlutusTx.unstableMakeIsData ''Proposal
+PlutusTx.makeLift ''Proposal
 
 data VoteType =  Approve | Reject 
-    deriving (Show, Enum)
+    deriving (Show, Generic, FromJSON, ToJSON, ToSchema, Enum)
+
+PlutusTx.unstableMakeIsData ''VoteType
+PlutusTx.makeLift ''VoteType
 
 data Vote = Vote {
     voter :: PubKeyHash,
     types  :: VoteType
-} deriving (Show)
+} deriving (Show, Generic, FromJSON, ToJSON, ToSchema, Enum)
 
-data Governance = Governance {
-    pVotesThreshold     :: Integer,
-    gVotesThreshold     :: Integer,
-    maxProposalPrice    :: Integer,
-    votersRewardPercent :: Integer
-} deriving (Show)
+PlutusTx.unstableMakeIsData ''Vote
+PlutusTx.makeLift ''Vote
 
 data IvyDatum = Genesis | IvyDatum {
     proposals :: [Proposal],
     votes     :: Map Integer Vote
-} deriving (Show)
+} deriving (Show, Generic, FromJSON, ToJSON, Enum)
 
+PlutusTx.unstableMakeIsData ''IvyDatum
+PlutusTx.makeLift ''IvyDatum
 
-ivyToken = "IVY"
+data IvyReedemer = RedeemWIP
+    deriving (Show, Generic, FromJSON, ToJSON, Enum)
 
--- Initial
+PlutusTx.unstableMakeIsData ''IvyReedemer
+PlutusTx.makeLift ''IvyReedemer
+
+ivyToken = "IVY_T1"
+
+-- Initial -------------------------
 defGovernance :: Governance
 defGovernance = Governance {
     pVotesThreshold     = 100,
@@ -126,17 +167,41 @@ defGovernance = Governance {
 }
 
 -- On-chain
-{-# INLINABLE mkPolicy #-}
-mkPolicy :: () -> ScriptContext -> Bool
-mkPolicy  _ _ = True
+{-# INLINABLE mkValidator #-}
+mkValidator :: IvyDatum -> IvyReedemer -> ScriptContext -> Bool
+mkValidator  _ _ _ = True
 
-policy :: Scripts.MintingPolicy
-policy = mkMintingPolicyScript $
-    $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy $ mkPolicy||])
 
-curSymbol :: CurrencySymbol
-curSymbol = scriptCurrencySymbol $ policy
+data IvyTokenContract
+instance Scripts.ValidatorTypes IvyTokenContract where
+    type instance DatumType IvyTokenContract = IvyDatum
+    type instance RedeemerType IvyTokenContract = IvyReedemer
 
+ivyTypedValidator :: Scripts.TypedValidator IvyTokenContract
+ivyTypedValidator = Scripts.mkTypedValidator @IvyTokenContract
+        $$(PlutusTx.compile [|| mkValidator ||])
+        $$(PlutusTx.compile [|| wrap ||])
+    where
+        wrap = Scripts.wrapValidator @IvyDatum @IvyReedemer
+
+-- ivyPolicy :: Scripts.MintingPolicy
+-- ivyPolicy = mkMintingPolicyScript $
+--         $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy mkValidator ||])
+--         $$(PlutusTx.compile [|| wrap ||])
+--     where
+--         wrap = Scripts.wrapValidator @IvyDatum @IvyReedemer
+
+-- curSymbol :: CurrencySymbol
+-- curSymbol = scriptCurrencySymbol $ ivyPolicy
+
+validator :: Validator
+validator = Scripts.validatorScript ivyTypedValidator
+
+valHash :: Ledger.ValidatorHash
+valHash = Scripts.validatorHash ivyTypedValidator
+
+scrAddress :: Ledger.Address
+scrAddress = scriptAddress validator
 
 -- Off-chain
 type IvyTokenSchema = Endpoint "mint" ()
@@ -148,10 +213,11 @@ mintIvy _ = do
     case Map.keys utxos of
         []       -> Contract.logError @String "no utxo found"
         oref : _ -> do
-            let val     = Value.singleton curSymbol ivyToken 1
-                lookups = Constraints.mintingPolicy policy <> Constraints.unspentOutputs utxos
+            -- TODO: Fix Value.singleton "aa" with the actual script adress
+            let val     = Value.singleton "aa" ivyToken 1
+                lookups = Constraints.typedValidatorLookups ivyTypedValidator <> Constraints.unspentOutputs utxos
                 tx      = Constraints.mustMintValue val <> Constraints.mustSpendPubKeyOutput oref
-            ledgerTx <- submitTxConstraintsWith @Void lookups tx
+            ledgerTx <- submitTxConstraintsWith lookups tx
             void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
             Contract.logInfo @String $ printf "forged %s" (show val)
 
